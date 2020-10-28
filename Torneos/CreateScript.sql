f468c4c52070d3e32491514f5c27dacf1b1c3487
Kevin Oswaldo Mejia Lemus <keosme@gmail.com>
2020-10-28 07:52:11 -0600

    microservicio de torneos

---
+++ Torneos/CreateScript.sql
@@ -0,0 +1,30 @@
+create table Juego(
+	id int auto_increment primary key,
+    nombre varchar(100),
+    ip varchar(50)
+);
+
+create table Torneo(
+	id int auto_increment primary key,
+    nombre varchar(100),
+    juegoid int,
+    ganadorid int,
+    llave int,
+    FOREIGN KEY (juegoid) REFERENCES Juego(id)
+);
+
+create table Participacion(
+	id int auto_increment primary key,
+    usuarioid int,
+    torneoid int,
+    FOREIGN KEY (torneoid) REFERENCES Torneo(id)
+);
+
+create table Partida(
+	id int auto_increment primary key,
+    jugador1 int,
+    jugador2 int,
+    llave int,
+    FOREIGN KEY (jugador1) REFERENCES Participacion(id),
+    FOREIGN KEY (jugador2) REFERENCES Participacion(id)
+);
