a2d9619010a4a08bed7f353817260d679d81c68a
Kevin Mejia <keosme@gmail.com>
2020-11-07 10:09:21 -0600

    update

--- Frontend/usuarios/js/env.js
+++ Frontend/usuarios/js/env.js
@@ -2,9 +2,9 @@ var ENDPOINT_USER = "http://18.191.208.8:3050";
 var ENDPOINT_TORNEOS = "http://34.67.137.28:3000";
 var ENDPOINT_TOKENS = "http://35.232.54.106";
 
-var ID_USER = "usuarios";
-var SECRET_USER = "usuarios-123";
-var ID_TORNEOS = "torneos";
-var SECRET_TORNEOS = "torneos-123";
+var ID_USER = "micro-juegos";
+var SECRET_USER = "secret-juegos-micro";
+var ID_TORNEOS = "micro-juegos";
+var SECRET_TORNEOS = "secret-juegos-micro";
 var ID_JUEGO = "Juego";
 var SECRET_JUEGOS = "JuegoSecret";
